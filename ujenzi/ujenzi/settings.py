7b1c0677cd8f1a60b29a211a9b0132080e1d3e24
kodalegit <victorkimani77@gmail.com>
2023-11-24 14:56:46 +0300

    set debug to false

--- ujenzi/ujenzi/settings.py
+++ ujenzi/ujenzi/settings.py
@@ -58,7 +58,7 @@ SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # Change this to "False" when you are ready for production
-DEBUG = True
+DEBUG = False
 
 
 # [START gaestd_py_django_csrf]
